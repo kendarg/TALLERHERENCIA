2e323cc9eb0590ec7b08a822d8f5e1c3af236c11
Kendarg Real <estebanreal2010@hotmail.com>
2026-07-10 11:51:02 -0500

    Arreglo con class punto 1

--- punto1.js
+++ punto1.js
@@ -1,12 +1,15 @@
-function restaurante(nombre,categoria,calificacion){
+class restaurante{
+    constructor (nombre,categoria,calificacion)
+{
 this.nombre = nombre;
 this.categoria = categoria;
 this.calificacion = calificacion;
 this.describir = function(){
-    return`Nombre: ${nombre} \nCategoria: ${categoria} \nCalificacion: ${calificacion}⭐ \n---------`
+    return`Nombre: ${this.nombre} \nCategoria: ${this.categoria} \nCalificacion: ${this.calificacion}⭐ \n---------`
 }
 this.estaBienCalificado = function(){
     return this.calificacion > 4.4;
+}
 }
 }
 const p = new restaurante("Corral","Hamburguesas",4.5);
